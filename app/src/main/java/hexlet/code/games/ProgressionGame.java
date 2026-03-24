ce9398540dfa9c1aef63a11a50b9f72ea740b401
Kirill Vasilev <kirillwork56@gmail.com>
2026-03-24 21:45:36 +0300

    ref progression logic in getCorrectAnswer method

--- app/src/main/java/hexlet/code/games/ProgressionGame.java
+++ app/src/main/java/hexlet/code/games/ProgressionGame.java
@@ -41,36 +41,32 @@ public final class ProgressionGame {
     }
 
     public static String getCorrectAnswer(String question) {
-        String[] numbers = question.split(" ");
+        int length = GameUtils.getRandomNumber(MIN_LENGTH_PROGRESSION, MAX_LENGTH_PROGRESSION);
+        int hiddenIndex = GameUtils.getRandomNumber(0, length - 1);
+        int step = GameUtils.getRandomNumber(1, MAX_STEP);
+        int startNumber = GameUtils.getRandomNumber(MIN_START_NUMBER, MAX_START_NUMBER);
 
-        int hiddenIndex = -1;
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i].equals("..")) {
-                hiddenIndex = i;
-                break;
+        return buildProgression(length, startNumber, step, hiddenIndex);
+    }
+
+    private static String buildProgression(int length, int startNumber, int step, int hiddenIndex) {
+        var progression = new StringBuilder();
+        int current = startNumber;
+
+        for (int i = 0; i < length; i++) {
+            if (i > 0) {
+                progression.append(" ");
             }
+
+            if (i == hiddenIndex) {
+                progression.append("..");
+            } else {
+                progression.append(current);
+            }
+
+            current += step;
         }
 
-        int step;
-        if (hiddenIndex >= 2) {
-            int a = Integer.parseInt(numbers[hiddenIndex - 1]);
-            int b = Integer.parseInt(numbers[hiddenIndex - 2]);
-            step = a - b;
-        } else {
-            int a = Integer.parseInt(numbers[hiddenIndex + 2]);
-            int b = Integer.parseInt(numbers[hiddenIndex + 1]);
-            step = a - b;
-        }
-
-        int result;
-        if (hiddenIndex > 0) {
-            int prevNumber = Integer.parseInt(numbers[hiddenIndex - 1]);
-            result = prevNumber + step;
-        } else {
-            int nextNumber = Integer.parseInt(numbers[hiddenIndex + 1]);
-            result = nextNumber - step;
-        }
-
-        return String.valueOf(result);
+        return progression.toString();
     }
 }
